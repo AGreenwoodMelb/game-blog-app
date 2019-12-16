61228e42e3e50316e1ddbed7df3c0cda3f3136f7
git <agreenwood2009@gmail.com>
2019-12-16 22:36:40 +1100

    Pre-clean-up deletes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
-    def index
+    def home
     end
+
+    def about
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'home#index'
+  root 'home#home'
+  get 'about', to: 'home#about'
   resources :posts
 
 
